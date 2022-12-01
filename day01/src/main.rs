e835cb6e750d4df60562d7a8fc6a0e530945eb4b
Zolhungaj <zolhungaj@gmail.com>
2022-12-01 09:32:28 +0100

    day 1 part 2 finish

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -7,6 +7,8 @@ use std::path::Path;
 fn main() {
     let result1 = part1();
     println!("Result 1: {result1}");
+    let result2 = part2();
+    println!("Result 2: {result2}");
 }
 
 fn part1() -> u32 {
@@ -23,6 +25,32 @@ fn part1() -> u32 {
         sum += line.parse::<u32>().unwrap();
     }
     calories.into_iter().max().unwrap()
+}
+
+fn part2() -> u32 {
+    let content = get_content("input1.txt".to_string());
+    let values = content.split('\n');
+    let mut calories: Vec<u32> = Vec::new();
+    let mut sum = 0;
+    for line in values {
+        if line.is_empty() {
+            calories.push(sum);
+            sum = 0;
+            continue;
+        }
+        sum += line.parse::<u32>().unwrap();
+    }
+    let mut top3 = vec![0, 0, 0];
+
+    for mut sum in calories {
+        for value in top3.iter_mut() {
+            if *value < sum {
+                std::mem::swap(&mut (*value), &mut sum);
+            }
+        }
+    }
+
+    top3.into_iter().sum()
 }
 
 fn get_content(input: String) -> String {
