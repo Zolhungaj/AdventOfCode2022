122929195b207b3030c70bc85fc7bb2014180455
Zolhungaj <zolhungaj@gmail.com>
2022-12-01 09:49:11 +0100

    day 1 cleanup

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -5,43 +5,20 @@ use std::io::Read;
 use std::path::Path;
 
 fn main() {
-    let result1 = part1();
+    let result1 = part1("input1.txt");
     println!("Result 1: {result1}");
-    let result2 = part2();
+    let result2 = part2("input1.txt");
     println!("Result 2: {result2}");
 }
 
-fn part1() -> u32 {
-    let content = get_content("input1.txt".to_string());
-    let values = content.split('\n');
-    let mut calories: Vec<u32> = Vec::new();
-    let mut sum = 0;
-    for line in values {
-        if line.is_empty() {
-            calories.push(sum);
-            sum = 0;
-            continue;
-        }
-        sum += line.parse::<u32>().unwrap();
-    }
+fn part1(filepath: &str) -> u32 {
+    let calories = get_calories(filepath);
     calories.into_iter().max().unwrap()
 }
 
-fn part2() -> u32 {
-    let content = get_content("input1.txt".to_string());
-    let values = content.split('\n');
-    let mut calories: Vec<u32> = Vec::new();
-    let mut sum = 0;
-    for line in values {
-        if line.is_empty() {
-            calories.push(sum);
-            sum = 0;
-            continue;
-        }
-        sum += line.parse::<u32>().unwrap();
-    }
+fn part2(filepath: &str) -> u32 {
+    let calories = get_calories(filepath);
     let mut top3 = vec![0, 0, 0];
-
     for mut sum in calories {
         for value in top3.iter_mut() {
             if *value < sum {
@@ -51,6 +28,22 @@ fn part2() -> u32 {
     }
 
     top3.into_iter().sum()
+}
+
+fn get_calories(filepath: &str) -> Vec<u32> {
+    let content = get_content(filepath.to_string());
+    let values = content.split('\n');
+    let mut calories: Vec<u32> = Vec::new();
+    let mut sum = 0;
+    for line in values {
+        if line.is_empty() {
+            calories.push(sum);
+            sum = 0;
+            continue;
+        }
+        sum += line.parse::<u32>().unwrap();
+    }
+    calories
 }
 
 fn get_content(input: String) -> String {
