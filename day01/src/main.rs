b42280b88081b0f18804e35e64f41b7a462c6333
Zolhungaj <zolhungaj@gmail.com>
2022-12-01 09:24:09 +0100

    day 1 part 1 finish

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,34 @@
+extern crate core;
+
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let result1 = part1();
+    println!("Result 1: {result1}");
+}
+
+fn part1() -> u32 {
+    let content = get_content("input1.txt".to_string());
+    let values = content.split('\n');
+    let mut calories: Vec<u32> = Vec::new();
+    let mut sum = 0;
+    for line in values {
+        if line.is_empty() {
+            calories.push(sum);
+            sum = 0;
+            continue;
+        }
+        sum += line.parse::<u32>().unwrap();
+    }
+    calories.into_iter().max().unwrap()
+}
+
+fn get_content(input: String) -> String {
+    let path = Path::new(&input);
+    let mut file = File::open(path).unwrap();
+    let mut content = String::new();
+    file.read_to_string(&mut content).unwrap();
+    content
 }
