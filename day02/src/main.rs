60998e691d4b4a838b380cfe720ac5734d04f6d0
Zolhungaj <zolhungaj@gmail.com>
2022-12-02 17:18:18 +0100

    remove duplicate code, loop into functional style
    Maybe a bit code-golfy idk

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -8,41 +8,42 @@ fn main() {
 }
 
 fn part_one(filepath: &str) -> i32 {
-    let content = get_content(filepath.to_string());
-    let values = content.split('\n');
-    let mut sum = 0;
-    for line in values {
-        if line.is_empty() {
-            continue;
-        }
-        let mut hands: Vec<&str> = line.split(' ').into_iter().collect();
-        let my_hand: Hand = hands.pop().unwrap().into();
-        let other_hand: Hand = hands.pop().unwrap().into();
-        sum += my_hand.round(&other_hand);
-    }
-    sum
+    get_inputs(filepath)
+        .into_iter()
+        .map(|(other_hand, my_hand): (String, String)| (other_hand.into(), my_hand.into()))
+        .map(|(other_hand, my_hand): (Hand, Hand)| my_hand.round(other_hand))
+        .sum()
 }
 
 fn part_two(filepath: &str) -> i32 {
+    get_inputs(filepath)
+        .into_iter()
+        .map(|(other_hand, strategy): (String, String)| (other_hand.into(), strategy))
+        .map(|(other_hand, strategy): (Hand, String)| {
+            (
+                other_hand,
+                match strategy.as_str() {
+                    "X" => other_hand.wins_against(),
+                    "Y" => other_hand.draws_against(),
+                    "Z" => other_hand.loses_against(),
+                    _ => panic!(),
+                },
+            )
+        })
+        .map(|(other_hand, my_hand): (Hand, Hand)| my_hand.round(other_hand))
+        .sum()
+}
+
+fn get_inputs(filepath: &str) -> Vec<(String, String)> {
     let content = get_content(filepath.to_string());
     let values = content.split('\n');
-    let mut sum = 0;
-    for line in values {
-        if line.is_empty() {
-            continue;
-        }
-        let mut hands: Vec<&str> = line.split(' ').into_iter().collect();
-        let strategy = hands.pop().unwrap();
-        let other_hand: Hand = hands.pop().unwrap().into();
-        let my_hand = match strategy {
-            "X" => other_hand.wins_against(),
-            "Y" => other_hand.clone(),
-            "Z" => other_hand.defeated_by(),
-            _ => panic!(),
-        };
-        sum += my_hand.round(&other_hand);
-    }
-    sum
+    values
+        .into_iter()
+        .filter(|line| !line.is_empty()) //empty lines are ignoreable
+        .map(|line| line.split(' ')) //divide into left and right
+        .map(|mut split| (split.next().unwrap(), split.next().unwrap())) //turn left and right input into tuple
+        .map(|(x, y)| (x.to_string(), y.to_string())) //create owned String to be able to return
+        .collect()
 }
 
 fn get_content(input: String) -> String {
@@ -69,7 +70,7 @@ impl Result {
     }
 }
 
-#[derive(PartialEq, Clone)]
+#[derive(PartialEq, Clone, Copy)]
 enum Hand {
     Rock,
     Paper,
@@ -77,13 +78,13 @@ enum Hand {
 }
 
 impl Hand {
-    fn round(&self, other_hand: &Hand) -> i32 {
-        self.result(other_hand).score() + self.intrinsic_value()
+    fn round(self, other_hand: Hand) -> i32 {
+        self.result(&other_hand).score() + self.intrinsic_value()
     }
     fn result(&self, other_hand: &Hand) -> Result {
-        if self.defeated_by() == *other_hand {
+        if self.loses_against() == *other_hand {
             Result::Lose
-        } else if other_hand.defeated_by() == *self {
+        } else if other_hand.loses_against() == *self {
             Result::Win
         } else {
             Result::Draw
@@ -96,7 +97,7 @@ impl Hand {
             Hand::Scissors => 3,
         }
     }
-    fn defeated_by(&self) -> Hand {
+    fn loses_against(&self) -> Hand {
         match self {
             Hand::Rock => Hand::Paper,
             Hand::Paper => Hand::Scissors,
@@ -109,6 +110,9 @@ impl Hand {
             Hand::Paper => Hand::Rock,
             Hand::Scissors => Hand::Paper,
         }
+    }
+    fn draws_against(&self) -> Hand {
+        *self
     }
 }
 
@@ -123,5 +127,11 @@ impl From<&str> for Hand {
             "Z" => Hand::Scissors,
             _ => panic!(),
         }
+    }
+}
+
+impl From<String> for Hand {
+    fn from(input: String) -> Self {
+        input.as_str().into()
     }
 }
