5777b64243c849ca678b2798b3cdae4c2d5cdb2a
Zolhungaj <zolhungaj@gmail.com>
2022-12-04 11:09:19 +0100

    day 4 part 1

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -4,6 +4,50 @@ use std::path::Path;
 
 fn main() {
     println!("Hello, world!");
+    println!("Part one: {}", part_one("input1.txt"));
+    println!("Part two: {}", part_two("input1.txt"));
+}
+
+fn part_one(filepath: &str) -> i32 {
+    let lines = get_lines(filepath);
+    let split: Vec<((i32, i32), (i32, i32))> = lines
+        .iter()
+        .map(|line| line.split(','))
+        .map(|mut split| (split.next().unwrap(), split.next().unwrap()))
+        .map(|(left, right): (&str, &str)| (left.split('-'), right.split('-')))
+        .map(|(mut left, mut right)| {
+            (
+                (left.next().unwrap(), left.next().unwrap()),
+                (right.next().unwrap(), right.next().unwrap()),
+            )
+        })
+        .map(|((left_low, left_high), (right_low, right_high))| {
+            (
+                (left_low.parse().unwrap(), left_high.parse().unwrap()),
+                (right_low.parse().unwrap(), right_high.parse().unwrap()),
+            )
+        })
+        .collect();
+    let mut count = 0;
+    for ((left_low, left_high), (right_low, right_high)) in split {
+        if (left_low <= right_low && left_high >= right_high)
+            || (right_low <= left_low && right_high >= left_high)
+        {
+            count += 1;
+        }
+    }
+    count
+}
+
+fn part_two(filepath: &str) -> i32 {
+    0
+}
+
+fn get_lines(filepath: &str) -> Vec<String> {
+    get_content(filepath.to_string())
+        .lines()
+        .map(|x| x.to_string())
+        .collect()
 }
 
 fn get_content(filepath: String) -> String {
