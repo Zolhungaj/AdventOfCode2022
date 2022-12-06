5ee9d9df45eb96ca14a24f501f6ff3afc560bd2b
Zolhungaj <zolhungaj@gmail.com>
2022-12-06 10:03:33 +0100

    day 6 day 1 attempt 1 doesn't work dn why

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,11 +1,42 @@
+use std::collections::VecDeque;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
 fn main() {
     println!("Hello, world!");
+    println!("part_one: {}", part_one("input1.txt"));
 }
 
+fn part_one(filepath: &str) -> usize {
+    let content = get_content(filepath.to_string());
+    let mut buffer: VecDeque<char> = VecDeque::new();
+    'outer: for (position, current) in content.split("").enumerate() {
+        println!("{}, {}", position, current);
+        if current.is_empty() {
+            continue;
+        }
+        if buffer.len() >= 4 {
+            buffer.pop_front();
+        }
+        buffer.push_back(current.chars().next().unwrap());
+        println!("{:?}, {}", buffer, buffer.len());
+        if buffer.len() == 4 {
+            let mut last = '\0';
+            for c in &buffer {
+                if last == '\0' {
+                    last = *c;
+                }
+                println!("{c}, {last}");
+                if *c != last {
+                    continue 'outer;
+                }
+            }
+            return position;
+        }
+    }
+    0
+}
 
 fn get_content(filepath: String) -> String {
     let path = Path::new(&filepath);
