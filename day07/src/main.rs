4cb6f63424fee45ad5761c4ed44df022f05b3b40
Zolhungaj <zolhungaj@gmail.com>
2022-12-07 12:15:51 +0100

    day 7 part 2

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -53,7 +53,47 @@ fn part_one(filepath: &str) -> i64 {
 }
 
 fn part_two(filepath: &str) -> i64 {
-    0
+    let content = get_content(filepath.to_string());
+    let mut root = Folder::new("/".to_string());
+    let mut current_folder: Vec<String> = Vec::new();
+    for line in content.lines() {
+        if line.starts_with('$') {
+            let command = extract_command(line);
+            match command.name.as_str() {
+                "cd" => {
+                    let argument = command.argument.unwrap();
+                    if argument == "/" {
+                        current_folder.clear();
+                    } else if argument == ".." {
+                        current_folder.pop();
+                    } else {
+                        current_folder.push(argument);
+                    }
+                }
+                "ls" => {} //doesn't really change anything
+                _ => panic!("Unexpected command {}", command.name),
+            }
+        } else if line.starts_with("dir") {
+            let name = line.strip_prefix("dir ").unwrap();
+            let folder = Folder::new(name.to_string());
+            let mut target_folder = &mut root;
+            for name in &current_folder {
+                target_folder = target_folder.folders.get_mut(name.as_str()).unwrap();
+            }
+            target_folder.add_folder(folder);
+        } else {
+            let mut split = line.split(' ');
+            let size: i64 = split.next().unwrap().parse().unwrap();
+            let name = split.next().unwrap().to_string();
+            let mut target_folder = &mut root;
+            let file = FileItem { name, size };
+            for name in &current_folder {
+                target_folder = target_folder.folders.get_mut(name.as_str()).unwrap();
+            }
+            target_folder.files.push(file);
+        }
+    }
+    calculate_smallest_that_frees_up_enough(&root)
 }
 
 fn extract_command(line: &str) -> Command {
@@ -75,6 +115,24 @@ fn calculate_size(root: &Folder) -> i64 {
     sum
 }
 
+fn calculate_smallest_that_frees_up_enough(root: &Folder) -> i64 {
+    let max: i64 = 70000000;
+    let target = 30000000;
+    let curr_size = root.size();
+    let required = target - (max - curr_size);
+    min_that_satisfies(root, required)
+}
+
+fn min_that_satisfies(root: &Folder, required: i64) -> i64 {
+    let mut min = i64::MAX;
+    if root.size() >= required {
+        min = root.size();
+    }
+    for (_, folder) in &root.folders {
+        min = min.min(min_that_satisfies(folder, required));
+    }
+    min
+}
 struct Folder {
     folders: HashMap<String, Folder>,
     files: Vec<FileItem>,
