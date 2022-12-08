37e0df128efc4430e4b4a393bb75eca6bd339299
Zolhungaj <zolhungaj@gmail.com>
2022-12-08 10:19:46 +0100

    day 8 part 2

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -15,14 +15,12 @@ fn part_one(filepath: &str) -> usize {
         for c in line.chars() {
             let c: String = c.into();
             let height: i32 = c.as_str().parse().unwrap();
-            print!("{height}");
             let tree = Tree {
                 height,
                 visible: false,
             };
             row.push(tree);
         }
-        println!();
         grid.push(row);
     }
     for vec in grid.iter_mut() {
@@ -45,12 +43,10 @@ fn part_one(filepath: &str) -> usize {
         let mut min = -1;
         for vec in &mut grid {
             let mut tree = vec.get_mut(x).unwrap();
-            print!("{} {} ->", tree.height, tree.visible);
             if tree.height > min {
                 min = tree.height;
                 tree.visible = true;
             }
-            println!("{}, min={}", tree.visible, min);
         }
         min = -1;
         for vec in grid.iter_mut().rev() {
@@ -61,31 +57,71 @@ fn part_one(filepath: &str) -> usize {
             }
         }
     }
-
-    for vec in &grid {
-        for tree in vec {
-            let c = if tree.visible { "1" } else { "0" };
-            print!("{c}");
-        }
-        println!()
-    }
-
-    for vec in &grid {
-        for tree in vec {
-            let c = tree.height;
-            print!("{c}");
-        }
-        println!()
-    }
-
     grid.into_iter()
         .flat_map(|vec| vec.into_iter())
         .filter(|tree| tree.visible)
         .count()
 }
 
-fn part_two(_filepath: &str) -> i32 {
-    0
+fn part_two(filepath: &str) -> i32 {
+    let content = get_content(filepath.to_string());
+    let mut grid: Vec<Vec<Tree>> = Vec::new();
+    for line in content.lines() {
+        let mut row: Vec<Tree> = Vec::new();
+        for c in line.chars() {
+            let c: String = c.into();
+            let height: i32 = c.as_str().parse().unwrap();
+            let tree = Tree {
+                height,
+                visible: false,
+            };
+            row.push(tree);
+        }
+        grid.push(row);
+    }
+    let mut best_scenic_score = -1;
+
+    for (tree_x, row) in grid.iter().enumerate() {
+        for (tree_y, tree) in row.iter().enumerate() {
+            let height = tree.height;
+            let mut scenic_score = 1;
+            let mut count = 0;
+            for y in tree_y + 1..row.len() {
+                count += 1;
+                if row.get(y).unwrap().height >= height {
+                    break;
+                }
+            }
+            scenic_score *= count;
+            count = 0;
+            for y in (0..tree_y).rev() {
+                count += 1;
+                if row.get(y).unwrap().height >= height {
+                    break;
+                }
+            }
+            scenic_score *= count;
+            count = 0;
+            for x in tree_x + 1..grid.len() {
+                count += 1;
+                if grid.get(x).unwrap().get(tree_y).unwrap().height >= height {
+                    break;
+                }
+            }
+            scenic_score *= count;
+            count = 0;
+            for x in (0..tree_x).rev() {
+                count += 1;
+                if grid.get(x).unwrap().get(tree_y).unwrap().height >= height {
+                    break;
+                }
+            }
+            scenic_score *= count;
+            best_scenic_score = best_scenic_score.max(scenic_score);
+        }
+    }
+
+    best_scenic_score
 }
 
 struct Tree {
