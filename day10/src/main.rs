b1f467066b0644e815853562066273be2662e7b5
Zolhungaj <zolhungaj@gmail.com>
2022-12-10 14:47:24 +0100

    day 10 part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -4,21 +4,13 @@ use std::path::Path;
 fn main() {
     println!("part_one: {}", part_one("input1.txt"));
     println!("part_one: {}", part_one("input2.txt"));
+    part_two("input1.txt");
+    println!();
+    part_two("input2.txt");
 }
 
 fn part_one(filepath: &str) -> i32 {
-    let instructions: Vec<Instruction> = get_content(filepath.to_string())
-        .lines()
-        .into_iter()
-        .map(|x| x.split(' '))
-        .map(|mut x| (x.next().unwrap(), x.next()))
-        .map(|(instruction, value)| match instruction {
-            "noop" => Instruction::NoOp,
-            "addx" => Instruction::AddX(value.unwrap().parse().unwrap()),
-            _ => panic!(),
-        })
-        .collect();
-    let mut cpu = CentralProcessingUnit::new(instructions);
+    let mut cpu = get_cpu(filepath);
     let mut sum = 0;
     let positions = vec![20, 60, 100, 140, 180, 220];
     while cpu.tick() {
@@ -33,6 +25,46 @@ fn part_one(filepath: &str) -> i32 {
         }
     }
     sum
+}
+
+fn part_two(filepath: &str) {
+    let mut cpu = get_cpu(filepath);
+
+    let mut screen: Vec<bool> = vec![false; 240];
+
+    loop {
+        let range = cpu.register_x - 1..=cpu.register_x + 1;
+        let current_position = (cpu.position as i32) % 40;
+        if range.contains(&current_position) {
+            if let Some(pixel) = screen.get_mut(cpu.position) {
+                *pixel = true;
+            }
+        }
+        if !cpu.tick() {
+            break;
+        }
+    }
+    for (count, value) in screen.into_iter().enumerate() {
+        if count % 40 == 0 {
+            println!()
+        }
+        print!("{}", if value { "#" } else { "." })
+    }
+}
+
+fn get_cpu(filepath: &str) -> CentralProcessingUnit {
+    let instructions: Vec<Instruction> = get_content(filepath.to_string())
+        .lines()
+        .into_iter()
+        .map(|x| x.split(' '))
+        .map(|mut x| (x.next().unwrap(), x.next()))
+        .map(|(instruction, value)| match instruction {
+            "noop" => Instruction::NoOp,
+            "addx" => Instruction::AddX(value.unwrap().parse().unwrap()),
+            _ => panic!(),
+        })
+        .collect();
+    CentralProcessingUnit::new(instructions)
 }
 
 fn get_content(filepath: String) -> String {
