8456a77eae6b88894d9319818ae7de10c808f84d
Zolhungaj <zolhungaj@gmail.com>
2022-12-14 18:40:43 +0100

    day 14 start

---
+++ day14/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+
+fn get_content(filepath: String) -> String {
+    let path = Path::new(&filepath);
+    let mut file = File::open(path).unwrap();
+    let mut content = String::new();
+    file.read_to_string(&mut content).unwrap();
+    content
+}
