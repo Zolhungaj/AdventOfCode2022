255aa4792e8df3bd9f010e3f4b259aa5452df638
Zolhungaj <zolhungaj@gmail.com>
2022-12-18 16:50:45 +0100

    day 18 part 1

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,7 +1,51 @@
+use std::collections::HashSet;
 use std::io::Read;
 
 fn main() {
-    println!("Hello, world!");
+    println!("part_one: {}", part_one("input2.txt"));
+    println!("part_one: {}", part_one("input1.txt"));
+    // println!("part_two: {}", part_two("input2.txt"));
+    // println!("part_two: {}", part_two("input1.txt"));
+}
+
+fn part_one(filepath: &str) -> i32 {
+    let cubes = extract_content(filepath);
+    let mut count = 0;
+    let directions = vec![
+        (-1, 0, 0),
+        (1, 0, 0),
+        (0, -1, 0),
+        (0, 1, 0),
+        (0, 0, -1),
+        (0, 0, 1),
+    ];
+    for cube in &cubes {
+        for direction in &directions {
+            let other_cube = (
+                cube.0 + direction.0,
+                cube.1 + direction.1,
+                cube.2 + direction.2,
+            );
+            if !cubes.contains(&other_cube) {
+                count += 1;
+            }
+        }
+    }
+    count
+}
+
+fn extract_content(filepath: &str) -> HashSet<(i32, i32, i32)> {
+    let mut hash_set = HashSet::new();
+    for line in get_content(filepath.to_string()).lines() {
+        let mut coords = line.split(',');
+        let cube: (i32, i32, i32) = (
+            coords.next().unwrap().parse().unwrap(),
+            coords.next().unwrap().parse().unwrap(),
+            coords.next().unwrap().parse().unwrap(),
+        );
+        hash_set.insert(cube);
+    }
+    hash_set
 }
 
 fn get_content(filepath: String) -> String {
